f35d29668531fa8eb2d03ce4a47f12e64f236f65
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2026-06-11 17:30:57 -0400

    Created Number Guessing Game, v1

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import random
 print("Hello, World!")
+num = random.randint(1, 10)
+print(f"The random number is: {num}")
+
+
+userGuess = input("Press Enter to continue...")
+userGuess = int(userGuess)
+
+if userGuess == num:
+    print("Congratulations! You guessed the number.")
+else:
+    while userGuess != num:
+        print(f"Your guess is: {userGuess}, the number is: {num}")
+        userGuess = int(input("Press Enter to continue..."))
+        if userGuess == num:
+            print("Congratulations! You guessed the number.")
+            break
+print(userGuess)
